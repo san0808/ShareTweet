23fb42080966c5597a084edb6c4fa5023c9e6dc6
SANKET BHAT <sanketbhat882002@gmail.com>
2023-02-25 19:26:42 +0530

    setting tailwind dependency to production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   important: true,
-//   // content: [
-//   //   "./pages/**/*.{js,ts,jsx,tsx}",
-//   //   "./components/**/*.{js,ts,jsx,tsx}",
-//   // ],
-//   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-  
-// }
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
