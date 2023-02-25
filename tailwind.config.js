b45acda106fa38326e58a731ac6ccd38e92d8a4c
SANKET BHAT <sanketbhat882002@gmail.com>
2023-02-25 19:09:07 +0530

    check-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   important: true,
+//   // content: [
+//   //   "./pages/**/*.{js,ts,jsx,tsx}",
+//   //   "./components/**/*.{js,ts,jsx,tsx}",
+//   // ],
+//   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+  
+// }
 module.exports = {
-  important: true,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-  enabled: process.env.NODE_ENV === "production",
-}
+};
