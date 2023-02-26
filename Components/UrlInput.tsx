c70209443355173a54de811021bed3f0ca99faad
SANKET BHAT <sanketbhat882002@gmail.com>
2023-02-26 18:46:46 +0530

    change in urlInput design

--- Components/UrlInput.tsx
+++ Components/UrlInput.tsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 import TweetBox from './TweetBox';
 
-
 function UrlInput() {
-    const [url, setUrl] = useState(' ');
-     
-    return (
-      <div className=" my-6 flex flex-col  items-center ">
-        <input
-          className="shadow-md w-4/12 p-2  border-slate-300 hover:border-lime-100 rounded-lg  "
-          type="text"
-          placeholder="Enter tweet URL"
-          value= {url}
-          onChange={e => setUrl(e.target.value)}
-        />
-        
-        <TweetBox Url={url} />
-      </div>
-    );
-  }
-  
-  export default UrlInput;
- 
+  const [url, setUrl] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
+
+  return (
+    <div className="my-4 md:my-8 flex flex-col items-center">
+      <label htmlFor="tweet-url" className="sr-only">
+        Enter tweet URL
+      </label>
+      <input
+        id="tweet-url"
+        className="shadow-md w-full max-w-md p-2 border-slate-300 hover:border-lime-100 rounded-lg flex-grow"
+        type="text"
+        placeholder="Enter tweet URL"
+        value={url}
+        onChange={handleInputChange}
+      />
+      <TweetBox Url={url} />
+    </div>
+  );
+}
+
+export default UrlInput;
