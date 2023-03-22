c56a9e4f0e6e88a4ff6029490b292f19e290cd8d
SANKET BHAT <sanketbhat882002@gmail.com>
2023-03-22 08:58:16 +0530

    fixed Url placeholder and color orientation

--- Components/ColorSelector.tsx
+++ Components/ColorSelector.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Card from './UI/Card';
-// import styles from '../styles/ColorSelector.css '
+
 type Props = {
   currentColor: string;
   onColorSelect: (color: string) => void;
@@ -8,23 +8,22 @@ type Props = {
 
 const ColorSelector: React.FC<Props> = ({ currentColor, onColorSelect }) => {
   const [isHovered, setIsHovered] = useState(false);
-  const colors = ['rgb(241 245 249)', 'rgb(254 242 242)', 'rgb(255 247 237)', 'rgb(247 254 231)', 'rgb(240 253 244)' , 'rgb(240 253 250)' ,'rgb(240 249 255)' ,' '];
-
+  const colors = ['rgb(241 245 249)', 'rgb(254 242 242)', 'rgb(255 247 237)', 'rgb(247 254 231)', 'rgb(240 253 244)', 'rgb(240 253 250)', 'rgb(240 249 255)', ' '];
 
   return (
-    
-    <div className="relative flex items-center justify-center h-10 w-10 hover:bg-green-50 " 
+    <div
+      className="relative flex items-center justify-center h-10 w-10 hover:bg-green-50 "
       onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}>
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div
-        className="rounded-full h-6 w-6 cursor-pointer before:box-border before:border-solid after:box-border after:border-solid "
+        className="rounded-full h-6 w-6 cursor-pointer "
         style={{ backgroundColor: currentColor }}
-        
       />
-      <div className="absolute top-10 left-0 z-10">
+      <div className="absolute top-10 left-0 z-10 ">
         <div
-          className={`mt-2 p-1 bg-white rounded-lg shadow-md   ${isHovered ? 'block' : 'hidden'}`}
-          style={{ minWidth: '2rem' }}
+          className={`mt-2 p-1 grid-auto-rows: max-content  bg-white rounded-lg shadow-md ${isHovered ? 'flex' : 'hidden'} w-fit `}
+          // style={{ minWidth: '2rem', display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
@@ -37,7 +36,10 @@ const ColorSelector: React.FC<Props> = ({ currentColor, onColorSelect }) => {
             />
           ))}
         </div>
-        <div className={`${isHovered ? 'block' : 'hidden'} absolute top-2 left-0 bottom-4 bg-white rounded-full h-4 w-4 flex-wrap`} style={{ zIndex: -1 }}></div>
+        <div
+          className={`${isHovered ? 'grid' : 'hidden'} absolute top-2 left-0 bottom-4 bg-white rounded-full h-4 w-4 `}
+          style={{ zIndex: -1 }}
+        ></div>
       </div>
     </div>
   );
@@ -46,5 +48,5 @@ const ColorSelector: React.FC<Props> = ({ currentColor, onColorSelect }) => {
 export default ColorSelector;
 
 
-const colors = ['rgb(241 245 249)', 'rgb(254 242 242)', 'rgb(255 247 237)', 'rgb(247 254 231)', 'rgb(240 253 244)' , 'rgb(240 253 250)' ,'rgb(240 249 255)' ,' '];
+
 
--- Components/UrlInput.tsx
+++ Components/UrlInput.tsx
@@ -3,12 +3,12 @@ import TweetBox from './TweetBox';
 
 
 function UrlInput() {
-    const [url, setUrl] = useState(' ');
+    const [url, setUrl] = useState('');
      
     return (
-      <div className=" my-6 flex flex-col  items-center ">
+      <div className=" my-6 flex flex-col  items-center  ">
         <input
-          className="shadow-md w-4/12 p-2  border-slate-300 hover:border-lime-100 rounded-lg  "
+          className="shadow-md w-4/12 p-2  border-slat-300 hover:border-lime-100 rounded-lg placeholder-gray-500 placeholder-opacity-75 "
           type="text"
           placeholder="Enter tweet URL"
           value= {url}
